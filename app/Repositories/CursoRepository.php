287cc7d4b9b847f8e9038968f8e19bc83d069ea2
andreyserbinario <andreyfbs2013@gmail.com>
2016-12-08 09:25:10 -0300

    Atualizações

---
+++ app/Repositories/CursoRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SerEducacional\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface CursoRepository
+ * @package namespace SerEducacional\Repositories;
+ */
+interface CursoRepository extends RepositoryInterface
+{
+    //
+}
