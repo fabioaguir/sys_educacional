55179af8e0e4c5fed5afd43fbc5f2df30067c84c
Felipe Lima <felipeldnascimento@gmail.com>
2016-12-08 09:28:09 +0000

    Progresso

---
+++ app/Entities/Telefone.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SerEducacional\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class Telefone extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    protected $table    = 'telefones';
+
+    protected $fillable = [
+        'nome',
+        'cgm_id'
+    ];
+
+}
